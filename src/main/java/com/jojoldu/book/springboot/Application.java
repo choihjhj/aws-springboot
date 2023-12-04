92a4506ff7fe9eb7121818c0402fe7af621d4657
choihyeji <hyejichoi14@gmail.com>
2023-12-05 02:02:42 +0900

    Application 필요없는 import 삭제

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,9 +1,7 @@
 package com.jojoldu.book.springboot;
 
-import hello.itemservice.ItemServiceApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing //JPA Auditing 활성화, jpa 엔티티 객체가 생성이 되거나 변경이 되었을 때 자동으로 값을 등록
