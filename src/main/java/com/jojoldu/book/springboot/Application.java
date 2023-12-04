7e0cc9d6ecd8dfb2beeb031e9e70761b6d95e2ae
choihyeji <hyejichoi14@gmail.com>
2023-12-04 16:42:10 +0900

    수정

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing //jpa 엔티티 객체가 생성이 되거나 변경이 되었을 때 자동으로 값을 등록
+@SpringBootApplication
+        //(exclude = SecurityAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
